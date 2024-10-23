a031dd765f005a46a7679e1c7d7297a45097fcb3
Junyi <zjy220124@alibaba-inc.com>
2024-10-23 14:27:43 +0800

    feat: add documentation by vitepress create

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  // 站点级选项
+  title: 'MetaSwitch',
+  description: '快捷地管理和切换多个代理设置',
+
+  themeConfig: {
+    logo: { src: '/images/icon48.png', width: 24, height: 24 },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/junyiz/MetaSwitch' }
+    ],
+  }
+}
