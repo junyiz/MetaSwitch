7d2770cf0e200305e22f97f78d89124b2831d35a
Junyi <zjy220124@alibaba-inc.com>
2024-11-07 17:43:18 +0800

    docs: add base path to VitePress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,7 @@ export default {
   description: '快捷地管理和切换多个代理设置',
 
   themeConfig: {
+    base: '/MetaSwitch/',
     logo: { src: '/images/icon48.png', width: 24, height: 24 },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/junyiz/MetaSwitch' }
