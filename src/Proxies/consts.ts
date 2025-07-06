5d4c91feb1e16bc9a95417a86d8d1d2e8f7c343a
junyiz <junyime@qq.com>
2025-07-06 10:18:38 +0800

    docs: 在注释里添加使用说明

--- src/Proxies/consts.ts
+++ src/Proxies/consts.ts
@@ -9,11 +9,16 @@ export const DEFAULT_RULE = `{
   "system": [],
   
   // 自定义代理，比如本机开发常用的 whistle
+  // 当启用自动切换（switjh profile）模式时，如下方括号里的域名，将走代理 whistle 
+  // 域名支持正则表达时，正则表达式用法见 https://junyiz.github.io/regexp/index.html
   "whistle": [
-    "local.dev"
+    "local.dev",
+    "^local.dev", // 这就是正则表达式，这仅会匹配 local.dev，不会匹配 pre-local.dev 
   ],
   
   // 自定义代理，比如科学上网常用的 v2ray
+  // 当启用自动切换（switch profile）模式时，如下方括号里的域名，将走代理 fixedProxy 
+  // 域名支持正则表达时，正则表达式用法见 https://junyiz.github.io/regexp/index.html
   "fixedProxy": [
     // "google.com"
   ],
