d6737b400e68cc69bdde93ce61f5f52e350f3a7b
junyiz <junyime@qq.com>
2025-07-06 11:00:11 +0800

    typo: switch

--- src/Proxies/consts.ts
+++ src/Proxies/consts.ts
@@ -9,7 +9,7 @@ export const DEFAULT_RULE = `{
   "system": [],
   
   // 自定义代理，比如本机开发常用的 whistle
-  // 当启用自动切换（switjh profile）模式时，如下方括号里的域名，将走代理 whistle 
+  // 当启用自动切换（switch profile）模式时，如下方括号里的域名，将走代理 whistle 
   // 域名支持正则表达时，正则表达式用法见 https://junyiz.github.io/regexp/index.html
   "whistle": [
     "local.dev",
