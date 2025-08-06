b91b7696e6243d234a1b2166e712a36be7b0871b
Junyi <junyime@qq.com>
2025-08-06 09:47:11 +0800

    feat(App.tsx): 添加字符串与Base64相互转换的工具函数 refactor(App.tsx): 重构导入导出功能，使用Base64格式存储数据

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,41 @@ import { ExportOutlined, FullscreenOutlined, ImportOutlined, EllipsisOutlined } 
 import Proxies from './Proxies/index'
 import './App.less'
 
+export const strToBase64 = function (str: string) {
+  const bytes = new TextEncoder().encode(str)
+  const binString = Array.from(bytes, (byte) => String.fromCodePoint(byte)).join('')
+  return window.btoa(binString)
+}
+
+export const base64ToStr = function (base64: string) {
+  const binString = window.atob(base64)
+  const bytes = Uint8Array.from(binString, (char) => char.codePointAt(0) as number)
+  const decoder = new TextDecoder()
+  return decoder.decode(bytes)
+}
 
 export default function App() {
   const onExport = () => {
     const a = document.createElement('a')
-    const json = Object.keys(localStorage).reduce((pre: Record<string, string | null>, key) => {
-      if (!key.startsWith('APLUS')) {
-        pre[key] = localStorage.getItem(key)
-      }
-      return pre
-    }, {})
-    a.href = `data:,${JSON.stringify(json, null, 2)}`
-    a.download = `MetaSwitch-${dayjs().format('YYYY-MM-DD')}.json`
+    const modes = localStorage.getItem('modes') as string
+    a.href = `data:,${strToBase64(modes)}`
+    a.download = `MetaSwitch-${dayjs().format('YYYY-MM-DD')}.txt`
     a.click()
   }
 
   const onImport = () => {
     const input = document.createElement('input')
     input.type = 'file'
-    input.accept = '.json'
+    input.accept = '.txt'
     input.onchange = (e) => {
       const file = (e.target as HTMLInputElement)?.files?.[0]
       if (file) {
         const reader = new FileReader()
         reader.readAsText(file)
         reader.onload = (e) => {
-          const json = JSON.parse(e.target?.result as string)
-          Object.keys(json).forEach((key) => {
-            if (!key.startsWith('APLUS')) {
-              localStorage.setItem(key, json[key])
-            }
-          })
+          const modes = e.target?.result as string
+          console.log(modes)
+          localStorage.setItem('modes', base64ToStr(modes))
           location.reload()
         }
       }
